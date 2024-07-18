ebf4aa14b9c8279b663960b6f072140fd430646b
Bhuvaneswari S <bhuvaneswarishanker2509@gmail.com>
2024-07-18 10:35:14 +0530

    Angular service worker example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { WebServiceWorker } from '../service/service-worker';
 
 @Component({
   selector: 'app-root',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
@@ -10,4 +13,33 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ang-service-worker';
-}
+  isNewAppVersionAvailable: boolean = false;
+  newAppUpdateAvailableSubscription: Subscription = new Subscription();
+
+
+  constructor(
+    private webServiceWorker: WebServiceWorker,
+  ) { }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+    this.checkIfAppUpdated();
+  }
+
+  checkIfAppUpdated() {
+    this.newAppUpdateAvailableSubscription = this.webServiceWorker.$isAnyNewUpdateAvailable.subscribe((versionAvailableFlag: boolean) => {
+      this.isNewAppVersionAvailable = versionAvailableFlag;
+    });
+  }
+
+  refreshApp() {
+    window.location.reload();
+  }
+
+  ngOnDestroy() {
+    this.newAppUpdateAvailableSubscription?.unsubscribe();
+  }
+} 
+
